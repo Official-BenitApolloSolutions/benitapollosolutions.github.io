701250a5bf7a665c9b36a8fb2fe6b234072b8832
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-15 01:47:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const getHome = function (){ 
+window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/home.html");
+console.log("200")
+console.log(location.protocol);
+    
+}
+
+export { getHome };
---
+++ message.js
@@ -0,0 +1,7 @@
+import { getHome } from 'https://eduqii-blip.github.io/benitapollosolutions/index.js';
+            
+window.addEventListener("load", (e) => {
+     e.preventDefault();
+                 
+    getHome();
+});
