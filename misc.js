f75f4882f5b2d874e503117d62deb950d687fd7e
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-21 15:08:21 +0000

    Add files via upload

--- misc.js
+++ misc.js
@@ -1,5 +1,10 @@
 const biztimer = document.querySelector("#businessday");
 const user = document.querySelector("#user");
 let newday = new Date();
-biztimer.innerHTML = newday.toDateString();
+window.addEventListener("load", (e) => {
+    e.preventDefault();
+    
+    biztimer.innerHTML = newday.toDateString();
 user.innerHTML = navigator.UserAgent();
+});
+
