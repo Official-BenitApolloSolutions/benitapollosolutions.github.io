a1f4b2a43d7fedad8cdc722fc9de31a5c44b84cc
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-22 07:29:19 +0000

    Add files via upload

--- misc.js
+++ misc.js
@@ -1,6 +1,5 @@
-let biztimer = document.querySelector("#businessday");
+/*let biztimer = document.querySelector("#businessday");
 let user = document.querySelector("#user");
-let loadlesson = document.querySelector("#loadlesson");
 let newday = new Date();
 window.addEventListener("load", (e) => {
     e.preventDefault();
@@ -8,11 +7,17 @@ window.addEventListener("load", (e) => {
     biztimer.innerHTML = newday.toDateString();
 user.innerHTML = navigator.UserAgent();
 });
+*/
 
-loadlesson.addEventListener("click", (e) => {
-         e.preventDefault();
-         
+function LoadLesson(){
+    let loadLesson = document.getElementById("loadLesson");
+if(loadLesson.addEventListener){
+    loadlesson.addEventListener("click", MyFunction, false);
+  }
+}
+
+function MyFunction(){
     document.WriteLine("loading lesson. please wait...");
     window.location.assign("https:/\/www.benitapollosolutions.com/lessonnotes1.html");
     console.log("i got executed!");
-});
+}
