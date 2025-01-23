222e12a7f20fcb5415264aa37c1826b863f22d00
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-23 21:32:21 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const getHome = function (){
 //window.location.assign("https:/\/www.benitapollosolutions.com/home.html");
 
     let usrld =   document.getElementById("loadstatus");
-    usrld.classList.add("invisible");
 
  console.log("200");
-console.log(location.protocol);
-    
+  setTimeOut(() => { 
+  usrld.classList.add("invisible");   console.log(location.protocol); 
+      }, 3000);
 }
 
 export { getHome };
