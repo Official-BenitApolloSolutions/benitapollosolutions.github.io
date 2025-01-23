0df063b266f7a7a2f05346483ee3740afa0de905
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-23 20:55:25 +0000

    Add files via upload

--- site.js
+++ site.js
@@ -4,11 +4,33 @@
              })();
              
           window.onload = function() {
+          
           let studyform = document.getElementById('study-form');
+          let solutionform = document.getElementById("solution-form");
+          
     if(studyform.addEventListener){
         // start of control
         
          studyform.addEventListener('submit', function(event) {
+                event.preventDefault();
+                // date
+                emailjs.sendForm('service_xga0ihl', 'template_66nw7jk', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+document.writeln("successfully sent a quote. Check your email for confirmation.");
+            location.reload();        
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        
+        //end of control
+    }
+    
+    if(solutionform.addEventListener){
+        // start of control
+        
+         solutionform.addEventListener('submit', function(event) {
                 event.preventDefault();
                 // date
                 emailjs.sendForm('service_xga0ihl', 'template_66nw7jk', this)
