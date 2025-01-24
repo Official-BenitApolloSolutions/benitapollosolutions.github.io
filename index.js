7a855b00fb4ffd4444ee2f71c635ae1e8048f6b1
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-24 12:20:48 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const GetHome = function (){
 //window.location.assign("https:/\/www.benitapollosolutions.com/home.html");
 
  console.log("200");
+ 
  let usrld =   document.getElementById("loadstatus");
   usrld.style.display = "block";
-   console.log(location.protocol);
-      
+   console.log(location.protocol);  
 }
 
 export { GetHome };
--- message.js
+++ message.js
@@ -6,9 +6,16 @@ window.onload = function(){
     if(window.addEventListener){
         window.addEventListener("load", (e) => {
      e.preventDefault();
-                 
+      
+      let mainacc = document.getElementById("mainaccount"); 
+      let usrld =   document.getElementById("loadstatus");
+          
      GetHome();
      
+     setTimeOut(() => { 
+          mainacc.style.display = "block";
+          usrld.style.display = "none"; 
+      }, 2000); 
 }, false);
 
     }
