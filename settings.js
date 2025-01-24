a6db13468380ac188d66c7531894f49762a131ed
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-24 14:11:05 +0000

    Add files via upload

--- settings.js
+++ settings.js
@@ -3,3 +3,15 @@ const RegisterNow = function(){
     document.writeln("loading please wait...");
     window.location.assign("https:/\/www.benitapollosolutions.com/register.html");
 }
+
+function formSent(){
+    let _fullname = document.getElementById("fullname");
+let _email = document.getElementById("email");
+let _password = document.getElementById("password");
+if(typeof(Storage) !== "undefined"){
+  localStorage.setItem("fullname",_fullname);
+  let username = localStorage.getKey("fullname");
+  alert(username);
+}
+}
+
