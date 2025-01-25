512a0158a391d5358fde70f1520ecf855979ce64
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-01-25 20:20:27 +0000

    Add files via upload

--- settings.js
+++ settings.js
@@ -4,17 +4,47 @@ const RegisterNow = function(){
 }
 
 function RegisterUser(){
-   /* let _fullname = document.getElementById("fullname");
-let _email = document.getElementById("email");
-let _password = document.getElementById("password");
+    let _fullname = document.getElementById("fullname").value;
+let _email = document.getElementById("email").value;
+let _password = document.getElementById("password").value;
 if(typeof(Storage) !== "undefined"){
-  localStorage.setItem("fullname", _fullname);
-  let username = localStorage.getKey("fullname");
-  alert(username);*/
-  alert("hello world");
+  localStorage.fullname = _fullname;
+  localStorage.email = _email;
+  localStorage.password = _password;
+  console.log(localStorage.fullname);
+  alert("successfully registered!");
+  location.reload();
+ }
+}
 
+function LoginUser(){
+    let email = localStorage.email;
+    let password = localStorage.password;
+    let _email = document.getElementById("email").value;
+    let _password = document.getElementById("password").value;
+    if(email === _email && password === _password){
+        document.writeln("Logins accepted. ");
+        document.writeln("Loading... please wait");
+        window.location.assign("https:/\/www.benitapollosolutions.com/home.html");
+    }
 }
 
 function ShowPassword(){
-    alert("hello world")
+    let _pass1 = document.getElementById("password");
+    let _pass2 = document.getElementById("confirm_password");
+    if(_pass1.type === "password" || _pass2.type === "password" ){
+        _pass1.type = "text";
+        _pass2.type = "text";
+    }else{
+        _pass1.type = "password";
+        _pass2.type = "password";
+    }
+}
+
+function RememberMe(){
+  let remm = document.userform.rememberme.checked;
+  if(typeof(Storage) !== "undefined"){
+      sessionStorage.user = navigator.UserAgent;
+      console.log(sessionStorage.user);
+  }
 }
