656ee90a25991271ca93f5a9c29c560d03863607
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-02-03 00:24:42 +0000

    Add files via upload

--- edusite.js
+++ edusite.js
@@ -1,11 +1,21 @@
-import { paymentmethod } from  'https:/\/www.benitapollosolutions.com/sitesettings.js';
+import { paymentmethod, MyTimeTable } from  'https:/\/www.benitapollosolutions.com/sitesettings.js';
 
 let paymentbtn = document.getElementById("payment-method");
+
+let timetable = document.getElementById("timetable");
 
 function pay(){
     paymentmethod();
 }
 
+function Showtimetable(){
+    MyTimeTable();
+}
+
 if(paymentbtn.addEventListener){
-    paymentbtn.addEventListener("click", pay, false)
+    paymentbtn.addEventListener("click", pay, false);
+}
+
+if(timetable.addEventListener){
+    timetable.addEventListener("click", Showtimetable, false);
 }
--- sitesettings.js
+++ sitesettings.js
@@ -3,5 +3,10 @@ const paymentmethod = function (){
     window.location.assign("https:/\/wa.me/+233500518387?text=Connect%20me%20to%20the%20payment%20method.");
 }
 
-export { paymentmethod };
+const MyTimeTable = function (){
+    document.writeln("Loading timetable, please wait...");
+    document.location.assign("https:/\/www.benitapollosolutions.com/lessonmodel.html");
+}
+
+export { paymentmethod, MyTimeTable };
 
