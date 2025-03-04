a5cd5864b32980e5886eaba2eebf3eb1e7a09d3b
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-03-04 10:26:49 +0000

    Add files via upload

--- businessettings.js
+++ businessettings.js
@@ -28,7 +28,7 @@ let firstweek = document.getElementById("first-week");
 let secondweek = document.getElementById("second-week");
 let thirdweek = document.getElementById("third-week");
 let fourthweek = document.getElementById("fourth-week");
-let currency = "GHS "
+let currency = "GHS"
 let febsum = document.getElementById("Feb-sum");
 eval("febsum.innerHTML = currency + 380");
 
@@ -71,7 +71,7 @@ function PayDay(){
 //session payment
 
         for(let i=0; i < payList.length; i++){
-            payList[i].innerHTML = "GHS" + PayRate;
+            payList[i].innerHTML = "GHS" + 0;
             
         };
       
