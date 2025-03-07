12f2fe8b2deb703fa30d60b5cca8fbdc635c98e0
Official-BenitApolloSolutions <benitapollosolutions@gmail.com>
2025-03-07 18:12:06 +0000

    Add files via upload

--- businotifications.js
+++ businotifications.js
@@ -1,10 +1,10 @@
 let postfeed = document.querySelectorAll(".noti-updates");
-
+let totNotstoday;
 function NumberOfNotifications(){
     
     if(localStorage != "undefined"){
         localStorage.setItem("total-notis", postfeed.length);
-        let totNotstoday = localStorage.getItem("total-notis");
+        totNotstoday = localStorage.getItem("total-notis");
     }  
      return totNotstoday;
 }
