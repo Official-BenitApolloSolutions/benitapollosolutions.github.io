479a7bba310f900b925db4acff269fe3850dfa5a
infospot <benitapollosolutions@gmail.com>
2025-08-28 16:28:37 +0000

    refactor

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+import  { codingBootcamp } from "./index.js";
+
+const _codingBootcamp = document.getElementById("coding-bootcamp");
+_codingBootcamp.addEventListener('click', codingBootcamp, false);
---
+++ js/index.js
@@ -0,0 +1,10 @@
+function moveToUrl(){
+    window.location.assign("./dashboard/index.html");
+}
+
+function codingBootcamp(){
+    document.writeln("Loading...");
+    moveToUrl();
+}
+
+export { codingBootcamp }
