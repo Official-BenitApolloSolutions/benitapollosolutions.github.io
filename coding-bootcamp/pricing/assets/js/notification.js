33dde53f2af357d80e59029f83a1eb9822ec2773
infospot <benitapollosolutions@gmail.com>
2025-09-01 03:44:23 +0000

    typ

--- coding-bootcamp/pricing/assets/js/notification.js
+++ coding-bootcamp/pricing/assets/js/notification.js
@@ -10,14 +10,24 @@
 // });
 
 
- var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
+ var notificationModal1 = new bootstrap.Modal(document.getElementById('notificationModal1'), {
+        keyboard: false
+  });
+
+   var notificationModal2 = new bootstrap.Modal(document.getElementById('notificationModal2'), {
         keyboard: false
   });
 
   window.onload = function() {
-    if(!localStorage.getItem('modalShown')) {
-      myModal.show();
+    if(!localStorage.getItem('modalShown1')) {
+      notificationModal1.show();
 
-      localStorage.setItem('modalShown', true);
+      localStorage.setItem('modalShown1', true);
+    }
+
+    if(!localStorage.getItem('modalShown2')) {
+      notificationModal2.show();
+
+      localStorage.setItem('modalShown2', true);
     }
   };
