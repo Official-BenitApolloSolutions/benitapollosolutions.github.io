ed6db7946273894c80ce1e26b03d4acf62897a1b
infospot <benitapollosolutions@gmail.com>
2025-09-01 03:09:26 +0000

    refactor

--- coding-bootcamp/pricing/assets/js/notification.js
+++ coding-bootcamp/pricing/assets/js/notification.js
@@ -1,10 +1,23 @@
-$(document).ready(function() {
-  // Check if the modal has been shown before
-  if (!localStorage.getItem('modalShown')) {
-    // Show the modal
-    $('#myModal').modal('show');
+// $(document).ready(function() {
+//   // Check if the modal has been shown before
+//   if (!localStorage.getItem('modalShown')) {
+//     // Show the modal
+//     $('#myModal').modal('show');
 
-    // Set the flag in local storage
-    localStorage.setItem('modalShown', true);
-  }
-});
+//     // Set the flag in local storage
+//     localStorage.setItem('modalShown', true);
+//   }
+// });
+
+
+ var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
+        keyboard: false
+  });
+
+  window.onload = function() {
+    if(!localStorage.getItem('modalShown')) {
+      myModal.show();
+
+      localStorage.setItem('modalShown', true);
+    }
+  };
