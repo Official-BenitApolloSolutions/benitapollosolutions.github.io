4f588acd82b1bc5b8f31b33a44f1a34ed2cb0729
infospot <benitapollosolutions@gmail.com>
2025-10-02 04:29:29 +0000

    Update index.js

--- features/index.js
+++ features/index.js
@@ -1,12 +1,12 @@
 const getStartedFunction = () => {
     const getStartedButton = document.getElementById("getStartedButton");
     getStartedButton.addEventListener("click", () => {
-        const userConfirmed = confirm('You are about to be redirected to our checkout page. Do you want to proceed?');
+        const userConfirmed = confirm('You are about to be redirected to our checkout page with our payment partner - Flutterwave. Do you want to proceed?');
         if (!userConfirmed) {
             return; // User cancelled the action
         }
-        window.location.href = 'https://flutterwave.com/donate/zchgr8kgmfgv'; // checkout page
-    });
+        window.open('https://flutterwave.com/donate/zchgr8kgmfgv', '_blank'); // open in new tab
+        });
 }
 
 export { getStartedFunction };
